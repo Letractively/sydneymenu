1bd93ee8af40fa5b16d660b32fc5389df7af25fe
xgao <xgao@localhost>
2011-11-18 22:24:40 +1100

    remove core component

--- main/core/config.py
+++
@@ -1,9 +0,0 @@
-class CONFIG:
-  SERVICES_PATH = "/var/www/sydneymenu/main/services/"
-  GMAIL_LOGIN = "zoyoeproject@gmail.com"
-  GMAIL_PASSWORD = "aab09jpg"
-  WEEK_DAY_NAME = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-  CSS_TEMPLATE_PATH = 'default'
-  LOG_PATH = '/var/www/sydneymenu/log/'
-  XSLT_FORM_PATH = '/var/www/sydneymenu/config/form.xslt'
-  XSLT_TEMPLATE_PATH = '/var/www/sydneymenu/config/template.xslt'
