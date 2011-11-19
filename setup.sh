401b8007a0f7a6bfb7eac88040a17722de614092
xgao <xgao@localhost>
2011-11-20 01:30:02 +1100

    finish enable new setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 echo "Welcome using sydney-scent project ..."
 
-uname -a
+uname | grep 'Darwin'
+
+wget="wget"
+if [[ $? -eq 0 ]] ; then
+  wget="curl -O"
+  echo 'Darwin'
+fi
 
 echo "Checking Django Version ..."
 python djv.py
@@ -11,6 +17,7 @@ fi
 
 echo "Clean folders ..."
 rm -rf pybb
+rm -rf zoyoe
 rm -rf django*
 rm -rf res.tar.gz
 rm -rf res
@@ -32,8 +39,16 @@ if [ $? -gt 0 ]; then
 fi
 echo " === Done ==="
 
+echo "Getting zoyoe"
+hg clone https://code.google.com/p/zoyoe/
+if [ $? -gt 0 ]; then
+  echo " === Can not clone django-common, check your network and mecrial installation!"
+  exit 1
+fi
+echo "=== Done ==="
+
 echo "Getting Res Folder"
-wget http://sydneymenu.googlecode.com/files/res.tar.gz
+$wget http://sydneymenu.googlecode.com/files/res.tar.gz
 if [ $? -gt 0 ]; then
   echo "Can not get res folder, check your internet connection ...";
   exit 1;
@@ -45,9 +60,15 @@ echo "Making Symbolic Links"
 cd ./main
 rm -f pybb
 rm -f common
+rm -f core
+rm -f xmlbase
+rm -f zoyoe
 ln -s ../pybb/pybb ./
 ln -s ../django-common/common ./
 ln -s ../res ./
+ln -s ../zoyoe/core ./core
+ln -s ../zoyoe/xmlbase ./xmlbase
+ln -s ../zoyoe/js ./js
 
 echo "Synchronize DB"
 python manage.py syncdb
