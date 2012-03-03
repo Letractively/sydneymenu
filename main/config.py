02f198d8a7c4cf0c5cd86e2006ec28af24029be9
xgao <xgao@localhost>
2012-03-03 14:02:01 +0800

    enable facebook login

--- main/config.py
+++ main/config.py
@@ -8,7 +8,9 @@ class CONFIG:
   XSLT_FORM_PATH = '/var/www/sydneymenu/config/form.xslt'
   XSLT_TEMPLATE_PATH = '/var/www/sydneymenu/config/template.xslt'
   FB_API_KEY = '360231047327032'
+  FB_COOKIE_USR = 'ZOYOE_FB_USR'
+  FB_COOKIE_TOKEN = 'ZOYOE_FB_TOKEN' 
   FB_API_SECRET = 'f1231ceb5b252f9f2b5ef0c3edc1dafe'
-  FG_REST_SERVER = 'http://api.facebook.com/reset'
+  FB_GRAPH_SERVER = 'https://graph.facebook.com/'
 
 
