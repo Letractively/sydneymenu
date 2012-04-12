238a848a64f792c80eccbef3e9d3c08bb3d0e2bc
xgao <xgao@localhost>
2012-04-12 22:00:14 +1000

    add data.py in garden

---
+++ main/garden/data.py
@@ -0,0 +1,13 @@
+# Create your views here.
+from user import * 
+from glue.forum import *
+from django.db.models import Q
+from garden.models import *
+from garden.mesh import *
+
+def Config(request):
+  PLANT_ICON_LIST = [{'name':'vegetable','imgs':[]},{'name':'flower','imgs':[]}]
+  PLANT_ICON_LIST[0]['imgs'].append({'name':'test','url':'/res/res_error.png'})
+  PLANT_ICON_LIST[0]['imgs'].append({'name':'amaranth','url':'/garden/gallery/image/amaranth.jpg'})
+  GINFO = {'backend':'internal','gallerys':PLANT_ICON_LIST}
+  return HttpResponse(json.dumps(GINFO))
