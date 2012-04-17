2444e23995fdf71a81153e6ff0e4139501a62619
xgao <xgao@localhost>
2012-04-17 23:55:18 +1000

    slightly change the config structure

--- main/garden/data.py
+++ main/garden/data.py
@@ -9,5 +9,5 @@ def Config(request):
   PLANT_ICON_LIST = [{'name':'vegetable','imgs':[]},{'name':'flower','imgs':[]}]
   PLANT_ICON_LIST[0]['imgs'].append({'name':'test','url':'/res/res_error.png'})
   PLANT_ICON_LIST[0]['imgs'].append({'name':'amaranth','url':'/garden/gallery/image/amaranth.jpg'})
-  GINFO = {'backend':'internal','gallerys':PLANT_ICON_LIST}
+  GINFO = {'backend':'internal','albums':PLANT_ICON_LIST}
   return HttpResponse(json.dumps(GINFO))
